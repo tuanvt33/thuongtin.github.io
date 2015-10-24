225f077b10c24f581516f94869468e9693fc9a3f
thuongtin <thuongtin@Thuongs-MacBook-Pro.local>
2015-10-24 09:00:17 +0700

    6

--- scripts/push.js
+++ scripts/push.js
@@ -1,6 +1,6 @@
 (function() {
 
-  navigator.serviceWorker.register('/service-worker2.js');
+  navigator.serviceWorker.register('/sw.js');
 
   function subscribe() {
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
