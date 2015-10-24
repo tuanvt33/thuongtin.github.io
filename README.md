3d758448cf0fccabef6d7827642b1dc60fe61a8c
thuongtin <thuongtin@Thuongs-MacBook-Pro.local>
2015-10-24 08:36:38 +0700

    Test 4

---
+++ README.md
@@ -0,0 +1,3 @@
+# Push Notifications
+
+Simple example of how to get started with Chrome's push notification feature. Check out my [blog post from May 27th](http://blog.derrickshowers.com/push-notifications/) for details.
---
+++ scripts/push-notifications.js
@@ -0,0 +1,24 @@
+(function() {
+
+  navigator.serviceWorker.register('/service-worker.js');
+
+  function subscribe() {
+    navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+      serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
+        console.log('pushing now enabled!');
+        console.log('curl --header "Authorization: key=AIzaSyBTuyGYpgaO1dyMeMr3xwlTJ1QB97XBv34" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\\"registration_ids\\":[\\"' + subscription.subscriptionId + '\\"]}"');
+
+        // we should probably change the button text
+        var theButton = document.querySelector('.the-button');
+        theButton.textContent = 'Disable Push Notifications';
+      });
+    });
+  }
+
+  // first let's add a listener to the button
+  var theButton = document.querySelector('.the-button');
+  theButton.addEventListener('click', function() {
+    subscribe();
+  });
+
+})();
