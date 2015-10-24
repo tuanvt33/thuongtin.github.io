a7bab1b4e104764ba20399d64f94468fc1345f12
thuongtin <thuongtin@Thuongs-MacBook-Pro.local>
2015-10-24 08:48:47 +0700

    1

--- scripts/push-notifications.js
+++ scripts/push-notifications.js
@@ -5,8 +5,6 @@
   function subscribe() {
     navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
       serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
-        console.log('pushing now enabled!');
-        console.log('curl --header "Authorization: key=AIzaSyBTuyGYpgaO1dyMeMr3xwlTJ1QB97XBv34" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\\"registration_ids\\":[\\"' + subscription.subscriptionId + '\\"]}"');
         console.log(subscription.endpoint);
 
         // we should probably change the button text
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,16 @@
 (function() {
-  self.addEventListener('push', function(event) {
-    console.log('push received');
-    self.registration.showNotification('Service Worker Example', {
-      body: 'An example of what the body of a service worker could show. In a working app, you would want this to be dynamic, but you\'d just do that within your JavaScript. Cool!',
-      icon: 'some-icon.png',
-      tag: 'unique-identifier-of-some-sort'
-    });
-  });
+  self.addEventListener('push', function(event) {  
+  console.log('Received a push message', event);
+
+  var title = 'Yay a message.';  
+  var body = 'We have received a push message.';  
+  var tag = 'simple-push-demo-notification-tag';
+
+  event.waitUntil(  
+    self.registration.showNotification(title, {  
+      body: body,  
+      tag: tag  
+    })  
+  );  
+});
 })();
