99a01ccffee987f9e41c8f0ca36d4a59f9cbf5a9
thuongtin <thuongtin@Thuongs-MacBook-Pro.local>
2015-10-24 08:41:27 +0700

    Log endpoint

--- scripts/push-notifications.js
+++ scripts/push-notifications.js
@@ -7,6 +7,7 @@
       serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true}).then(function(subscription) {
         console.log('pushing now enabled!');
         console.log('curl --header "Authorization: key=AIzaSyBTuyGYpgaO1dyMeMr3xwlTJ1QB97XBv34" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\\"registration_ids\\":[\\"' + subscription.subscriptionId + '\\"]}"');
+        console.log(subscription.endpoint);
 
         // we should probably change the button text
         var theButton = document.querySelector('.the-button');
