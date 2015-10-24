b58eda9627176b778eafa51449d1a067ac19261f
thuongtin <thuongtin@Thuongs-MacBook-Pro.local>
2015-10-24 15:24:19 +0700

    t2

--- push7/push.js
+++ push7/push.js
@@ -80,7 +80,7 @@ function subscribe() {
   pushButton.disabled = true;
 
   navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {  
-    serviceWorkerRegistration.pushManager.subscribe()  
+    serviceWorkerRegistration.pushManager.subscribe({userVisibleOnly: true})  
       .then(function(subscription) {  
         // The subscription was successful  
         isPushEnabled = true;  
